57ead42f9f7c89cbde9f7dffe9fc1d9f2ba51fab
y-komori <komo@littleforest.jp>
2006-05-25 15:57:28 +0000

    ・DTDによるバリデーションを追加.

--- s2jface/src/main/java/org/seasar/jface/template/TemplateBuilder.java
+++ s2jface/src/main/java/org/seasar/jface/template/TemplateBuilder.java
@@ -18,24 +18,28 @@ import org.seasar.jface.component.impl.TemplateComponent;
 
 /**
  * @author y-komori
- *
+ * 
  */
 public class TemplateBuilder {
+    public static final String PUBLIC_ID_01 = "-//SEASAR//DTD S2JFace 0.1//EN";
+
+    public static final String DTD_PATH_01 = "org/seasar/jface/template/s2jface01.dtd";
+
     protected ResourceResolver resourceResolver = new ClassPathResourceResolver();
+
     protected TemplateTagHandlerRule rule = new TemplateTagHandlerRule();
-    
-    public TemplateComponent build(final String path){
+
+    public TemplateComponent build(final String path) {
         final SaxHandlerParser parser = createSaxHandlerParser(path);
         final InputStream is = getInputStream(path);
         try {
             return (TemplateComponent) parser.parse(is, path);
-        }
-        finally {
+        } finally {
             InputStreamUtil.close(is);
         }
-        
+
     }
-    
+
     protected InputStream getInputStream(final String path) {
         final InputStream is = resourceResolver.getInputStream(path);
         if (is == null) {
@@ -43,15 +47,15 @@ public class TemplateBuilder {
         }
         return is;
     }
-    
+
     protected SaxHandlerParser createSaxHandlerParser(final String path) {
         final SAXParserFactory factory = SAXParserFactoryUtil.newInstance();
-        //factory.setValidating(true);
+        factory.setValidating(true);
 
         final SAXParser saxParser = SAXParserFactoryUtil.newSAXParser(factory);
 
         final SaxHandler handler = new SaxHandler(rule);
-        //handler.registerDtdPath(PUBLIC_ID, DTD_PATH);
+        handler.registerDtdPath(PUBLIC_ID_01, DTD_PATH_01);
 
         final TagHandlerContext ctx = handler.getTagHandlerContext();
         ctx.addParameter("path", path);
