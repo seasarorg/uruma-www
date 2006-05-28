ca441fe3e0c7d0a61155cce163813e3fc81b4fe1
y-komori <komo@littleforest.jp>
2006-05-28 06:26:42 +0000

    ・エンコードがおかしくなっていたのを修正.

--- s2jface/src/test/java/org/seasar/jface/impl/TemplateWindowTest.java
+++ s2jface/src/test/java/org/seasar/jface/impl/TemplateWindowTest.java
@@ -1,9 +1,28 @@
-﻿package org.seasar.jface.impl;
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+ package org.seasar.jface.impl;
 
 import org.seasar.framework.unit.S2FrameworkTestCase;
 import org.seasar.jface.component.impl.TemplateComponent;
 import org.seasar.jface.template.TemplateBuilder;
 
+/**
+ * @author y-komori
+ *
+ */
 public class TemplateWindowTest extends S2FrameworkTestCase {
     public void test() {
         TemplateBuilder builder = new TemplateBuilder();
