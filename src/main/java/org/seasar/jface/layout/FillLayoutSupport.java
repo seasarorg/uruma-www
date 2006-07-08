d36fa0d334d583eeabb11235f605fafeca9b5281
y-komori <komo@littleforest.jp>
2006-07-08 10:19:37 +0000

    ・org.seasar.jface.layoutパッケージのテストクラス追加とそれに伴う修正.

--- src/main/java/org/seasar/jface/layout/FillLayoutSupport.java
+++ src/main/java/org/seasar/jface/layout/FillLayoutSupport.java
@@ -15,33 +15,82 @@
  */
 package org.seasar.jface.layout;
 
+import java.util.Map;
+
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Layout;
+import org.seasar.framework.util.StringUtil;
 import org.seasar.jface.component.impl.ControlComponent;
+import org.seasar.jface.util.PropertyUtil;
 
 /**
- * @author y-komori
+ * <code>FillLayout</code>の生成をサポートする<code>LayoutSupport</code>クラスです。</br>
  * 
+ * @author y-komori
+ * @see org.eclipse.swt.layout.FillLayout
  */
 public class FillLayoutSupport extends
         AbstractLayoutSupport<FillLayout, Object> {
     private static final String LAYOUT_NAME = "fill";
 
+    private static final String MARGIN_LAYOUT_PARAM = "margin";
+
+    private static final String MARGIN_HEIGHT_ATTR = "marginHeight";
+
+    private static final String MARGIN_WIDTH_ATTR = "marginWidth";
+
+    /*
+     * @see org.seasar.jface.layout.LayoutSupport#createLayout()
+     */
     public FillLayout createLayout() {
         return new FillLayout();
     }
 
+    /*
+     * @see org.seasar.jface.layout.LayoutSupport#getLayoutType()
+     */
     public Class<FillLayout> getLayoutType() {
         return FillLayout.class;
     }
 
+    /*
+     * @see org.seasar.jface.layout.LayoutSupport#createLayoutData()
+     */
     public Object createLayoutData() {
         return null;
     }
 
+    /*
+     * @see org.seasar.jface.layout.LayoutSupport#createLayoutData(org.seasar.jface.component.impl.ControlComponent)
+     */
     public Object createLayoutData(ControlComponent controlComponent) {
         return null;
     }
 
+    /**
+     * 独自のレイアウトパラメータ処理を行います。</br>
+     * <p>
+     * <code>margin</code> が指定されていた場合、<code>marginHeight</code>、
+     * <code>marginWidth</code> をすべて同じ値に設定します。
+     * </p>
+     * 
+     * @see org.seasar.jface.layout.AbstractLayoutSupport#setupAdditionalLayoutParam(org.eclipse.swt.widgets.Layout,
+     *      java.util.Map)
+     */
+    @Override
+    protected void setupAdditionalLayoutParam(final Layout layout,
+            final Map<String, String> layoutParams) {
+        String marginStr = layoutParams.get(MARGIN_LAYOUT_PARAM);
+        if (!StringUtil.isEmpty(marginStr)) {
+            int margin = Integer.parseInt(marginStr);
+            PropertyUtil.setField(layout, MARGIN_HEIGHT_ATTR, margin);
+            PropertyUtil.setField(layout, MARGIN_WIDTH_ATTR, margin);
+        }
+    }
+
+    /*
+     * @see org.seasar.jface.layout.LayoutSupport#getLayoutName()
+     */
     public String getLayoutName() {
         return LAYOUT_NAME;
     }
