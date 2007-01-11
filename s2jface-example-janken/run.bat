31a8c9d2d3f79b0a134dec8d644af8fb33d114a5
bskuroneko <bskuroneko@39733341-eb55-4435-b903-bb6451dae853>
2007-01-11 15:33:02 +0000

    ・jankenサンプルを別プロジェクトとして作成

---
+++ s2jface-example-janken/run.bat
@@ -0,0 +1,1 @@
+javaw -Dlibrary.path=. -jar s2-jface-example-janken-0.2.0.jar
---
+++ s2jface-example-janken/src/main/java/org/seasar/jface/example/janken/impl/RandomTactics.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.jface.example.janken.impl;
+
+import org.seasar.jface.example.janken.Janken;
+import org.seasar.jface.example.janken.Tactics;
+
+/**
+ * @author y-komori
+ * 
+ */
+public class RandomTactics implements Tactics {
+    public int readTactics() {
+        int hand = 0;
+
+        double randomNum = Math.random() * 3;
+        if (randomNum < 1) {
+            hand = Janken.STONE;
+        } else if (randomNum < 2) {
+            hand = Janken.SCISSORS;
+        } else if (randomNum < 3) {
+            hand = Janken.PAPER;
+        }
+
+        return hand;
+    }
+}
