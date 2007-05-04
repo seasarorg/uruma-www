c777930a97aac44d702933f3dcd8e5d9711777f1
y-komori <komo@littleforest.jp>
2007-05-04 14:30:37 +0000

    ・RCP対応開発開始.

---
+++ s2jface/src/main/java/org/seasar/eclipse/rcp/S2RcpPlugin.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.eclipse.rcp;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+import org.seasar.eclipse.common.util.LogUtil;
+
+/**
+ * @author y-komori
+ * 
+ */
+public class S2RcpPlugin extends Plugin {
+
+    // The shared instance.
+    private static S2RcpPlugin plugin;
+
+    /**
+     * The constructor.
+     */
+    public S2RcpPlugin() {
+        plugin = this;
+    }
+
+    /**
+     * This method is called upon plug-in activation
+     */
+    public void start(BundleContext context) throws Exception {
+    }
+
+    /**
+     * This method is called when the plug-in is stopped
+     */
+    public void stop(BundleContext context) throws Exception {
+        super.stop(context);
+        plugin = null;
+    }
+
+    /**
+     * Returns the shared instance.
+     */
+    public static S2RcpPlugin getDefault() {
+        return plugin;
+    }
+
+    public static void log(String msg) {
+        LogUtil.log(getDefault(), msg);
+    }
+
+    public static void log(Throwable throwable) {
+        LogUtil.log(getDefault(), throwable);
+    }
+}
